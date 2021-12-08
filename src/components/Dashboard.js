334bccabafc9d0395b763c136babfa20909f427e
Fatuma <sfatex@gmail.com>
2021-12-08 18:43:39 -0500

    Bind this so that the onSelect function behaves as expected

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -35,11 +35,10 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-     focused: id
-    });
-   }
-     
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
@@ -57,7 +56,7 @@ class Dashboard extends Component {
       id={panel.id}
       label={panel.label}
       value={panel.value}
-      onSelect={this.selectPanel}
+      onSelect={event=>this.selectPanel(panel.id)}
      />
     ));
 
